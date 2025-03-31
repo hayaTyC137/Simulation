2909961b794214d106db705b2529dc987e340a84
hayaTy <kigore17@mail.ru>
2025-04-01 00:57:02 +0300

    Создан класс main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Coordinates coordinates = new Coordinates(5, 10);
+        GameMap map = new GameMap();
+        map.putEntity(Creature.spawnRandomEntity(Coordinates.getRandomCoordinate(coordinates)));
+        map.RenderMap();
+        Creature.getTypeOfCreature(map,coordinates).makeMove(map);
+        System.out.println();
+        map.RenderMap();
     }
 }

f3ad710ca9df59e0351bd73e2c94667d10a3afdd
hayaTy <kigore17@mail.ru>
2025-04-09 11:08:24 +0300

    Добавлен класс травы

---
+++ src/Entity/StaticEntity/Grass.java
@@ -0,0 +1,13 @@
+package Entity.StaticEntity;
+
+import Entity.Entity;
+import Entity.TypeOfEntity;
+import GameMap.Coordinates;
+
+public class Grass extends Entity {
+
+    public Grass(Coordinates coordinates, TypeOfEntity typeOfEntity) {
+        super(coordinates, typeOfEntity);
+    }
+
+}

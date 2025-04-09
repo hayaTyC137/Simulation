e9303c6eaa81e763f4fcc414bccda3b7c4db736e
hayaTy <kigore17@mail.ru>
2025-04-09 11:08:39 +0300

    Добавлен класс камня

---
+++ src/Entity/StaticEntity/Rock.java
@@ -0,0 +1,12 @@
+package Entity.StaticEntity;
+
+import Entity.Entity;
+import Entity.TypeOfEntity;
+import GameMap.Coordinates;
+
+public class Rock extends Entity {
+
+    public Rock(Coordinates coordinates, TypeOfEntity typeOfEntity) {
+        super(coordinates, typeOfEntity);
+    }
+}

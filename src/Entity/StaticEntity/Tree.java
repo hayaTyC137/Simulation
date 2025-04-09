871e2ecf491016704ead27006d270cabf756425c
hayaTy <kigore17@mail.ru>
2025-04-09 11:08:53 +0300

    Добавлен класс дерева

---
+++ src/Entity/StaticEntity/Tree.java
@@ -0,0 +1,13 @@
+package Entity.StaticEntity;
+
+import Entity.Entity;
+import Entity.TypeOfEntity;
+import GameMap.Coordinates;
+
+public class Tree extends Entity {
+
+
+    public Tree(Coordinates coordinates, TypeOfEntity typeOfEntity) {
+        super(coordinates, typeOfEntity);
+    }
+}
